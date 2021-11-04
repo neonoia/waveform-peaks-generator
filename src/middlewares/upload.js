c40b611d43190ed70372de1cff34232028bafd68
Hafizh Budiman <hafizh@datasaur.ai>
2021-11-04 15:14:06 +0700

    Handle mp3 mimetype error

--- src/middlewares/upload.js
+++ src/middlewares/upload.js
@@ -1,16 +1,17 @@
-const multer = require('multer');
-const os = require('os');
-const mime = require('mime-types');
+const multer = require("multer");
+const os = require("os");
+const mime = require("mime-types");
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
     cb(null, `${os.homedir()}/audio`);
   },
   filename: (req, file, cb) => {
-    const ext = mime.extension(file.mimetype);
+    let ext = mime.extension(file.mimetype);
+    ext = ext === "mpga" ? "mp3" : ext;
     cb(null, `${file.fieldname}-${Date.now()}.${ext}`);
   },
 });
-const upload = multer({ storage }).single('file');
+const upload = multer({ storage }).single("file");
 
 module.exports = upload;
