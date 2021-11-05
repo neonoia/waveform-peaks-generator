487571b8f5cd0d428737ea28adc93c54393e7b59
Hafizh Budiman <hafizh@datasaur.ai>
2021-11-05 14:41:07 +0700

    Update env.example and joi default port configuration

--- src/config/config.js
+++ src/config/config.js
@@ -1,17 +1,21 @@
-const dotenv = require('dotenv');
-const path = require('path');
-const Joi = require('joi');
+const dotenv = require("dotenv");
+const path = require("path");
+const Joi = require("joi");
 
-dotenv.config({ path: path.join(__dirname, '../../.env') });
+dotenv.config({ path: path.join(__dirname, "../../.env") });
 
 const envVarsSchema = Joi.object()
   .keys({
-    NODE_ENV: Joi.string().valid('production', 'development', 'test').required(),
-    PORT: Joi.number().default(3000),
+    NODE_ENV: Joi.string()
+      .valid("production", "development", "test")
+      .required(),
+    PORT: Joi.number().default(5005),
   })
   .unknown();
 
-const { value: envVars, error } = envVarsSchema.prefs({ errors: { label: 'key' } }).validate(process.env);
+const { value: envVars, error } = envVarsSchema
+  .prefs({ errors: { label: "key" } })
+  .validate(process.env);
 
 if (error) {
   throw new Error(`Config validation error: ${error.message}`);
